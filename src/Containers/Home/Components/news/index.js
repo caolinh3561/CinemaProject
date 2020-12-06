7d8cb5ba32169887d7ef492c300e181d1df177b1
BaoNguyen081296 <baonguyen081296@gmail.com>
2020-12-06 20:56:56 +0700

    update Home - Component News

--- src/Containers/Home/Components/news/index.js
+++ src/Containers/Home/Components/news/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./news.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 export default function News() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
   return (
     <section className="news">
       <div className="wrapper">
@@ -38,7 +51,21 @@ export default function News() {
                 </p>
               </div>
             </div>
-            <div className="col-sm-6 news__right"></div>
+            <div className="col-sm-6 news__right text-center">
+              <img
+                src="/img/mobilenews/mobilenews.png"
+                height={490}
+                width={195}
+                alt="Loading"
+              />
+              <Slider className="text-center news__slider" {...settings}>
+                <div className="news__img1 news__img img-fluid"></div>
+                <div className="news__img2 news__img img-fluid"></div>
+                <div className="news__img3 news__img img-fluid"></div>
+                <div className="news__img4 news__img img-fluid"></div>
+                <div className="news__img5 news__img img-fluid"></div>
+              </Slider>
+            </div>
           </div>
         </div>
       </div>
