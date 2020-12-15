7b4e6af372973769cfec9eda1f2b41cb5c80cb39
caolinh3561 <caobalinh3561@gmail.com>
2020-12-15 14:14:43 +0700

    update code

--- src/Containers/Home/Components/footer/index.js
+++ src/Containers/Home/Components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.scss";
 export default function FooterComponent() {
-  const img = "/img/iconsfooter/";
+  const img = "img/iconsfooter/";
   return (
     <footer>
       <div className="container">
